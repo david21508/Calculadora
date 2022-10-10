07b0a8e132fc6cd1b2a26c6296300fa4cfe41a7a
Davide Aguirre <davide.aguirrech@amigo.edu.co>
2022-10-10 11:18:36 -0500

    mi primer commit de la calculadora

---
+++ index.js
@@ -0,0 +1,12 @@
+function multiplicar() {
+    let num1 = document.getElementById("x").value;
+    let num2 = document.getElementById("y").value;
+    document.getElementById("resultado").innerHTML = num1 * num2;
+}
+
+function dividir() {
+    let num1 = document.getElementById("x").value;
+    let num2 = document.getElementById("y").value;
+    document.getElementById("resultado").innerHTML = num1 / num2;
+
+}
